946e7cbce8f42294fe648aba7b68371214225d72
tano.vanlerberghe <tano.vanlerberghe@student.howest.be>
2024-07-12 16:53:40 +0200

    Added all questions and category api endpoints

--- src/main/java/org/example/quiz/controller/QuestionController.java
+++ src/main/java/org/example/quiz/controller/QuestionController.java
@@ -1,16 +1,30 @@
 package org.example.quiz.controller;
 
+import org.example.quiz.Question;
+import org.example.quiz.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 
 @RequestMapping("question")
 public class QuestionController {
 
+    @Autowired
+    QuestionService questionService;
+
     @GetMapping("allQuestions")
-    public String getAllQuestions(){
+    public List<Question> getAllQuestions(){
         return questionService.getAllQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable String category){
+        return questionService.getQuestionsByCategory(category);
     }
 }
--- src/main/java/org/example/quiz/service/QuestionService.java
+++ src/main/java/org/example/quiz/service/QuestionService.java
@@ -1,4 +1,23 @@
 package org.example.quiz.service;
 
+
+import org.example.quiz.Question;
+import org.example.quiz.dao.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class QuestionService {
+    @Autowired
+    QuestionDao questionDao;
+
+    public List<Question> getAllQuestions() {
+        return questionDao.findAll();
+    }
+
+    public List<Question> getQuestionsByCategory(String category) {
+        return questionDao.findByCategory(category);
+    }
 }
